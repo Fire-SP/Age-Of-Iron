3f5ad3341930d1a9c018d8941a198ffbb11a47d8
Fire-SP <36900064+Fire-SP@users.noreply.github.com>
2018-04-20 16:01:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,12 @@ size = MaxX * MaxY
 tileSize = 10
 focusSize = 10
 
+inventory = [100,100,100,0,0,0]
+#           Wood,Stone,Food,Copper,Tin,Iron
+
 focusX = 0
 focusY = 0
+
 
 land = []
 oreGEN = []
@@ -36,8 +40,10 @@ GRASS = 2
 FOREST = 3
 MOUNTAIN = 4
 
-COLORS = [(46, 164, 223), (200,180,160), (82, 127, 25), (53, 76, 25), (230, 230, 230)]
+COLORS = [(46, 164, 223), (180,160,140), (82, 127, 25), (53, 76, 25), (230, 230, 230)]
 LABELS = ["Water", "Sand", "Grass", "Forest", "Mountain"]
+
+SelectImage = pygame.image.load("SelectionCursor.png")
 
 # Helper functions
 def doubleToSingle(x, y):
@@ -106,35 +112,75 @@ class GUI(): # Draws GUI, Very simple right now
     def Render():
         pygame.draw.rect(screen, (100,100,100), (1000,0, 300, 1000))
         pygame.draw.rect(screen, (150,150,150), (1000,0, 10, 1000))
+        player.PrintResource()
+
+    def RightClick():
+        
+        mouseX, mouseY = pygame.mouse.get_pos()
+        OnScreenRender()
+        pygame.draw.rect(screen,(100,100,100),(int(mouseX),int(mouseY),30,50))
+        pygame.draw.rect(screen,(255,50,50),(int(mouseX)+25,int(mouseY),5,5))
+        
+        #This Is Broken
 
 class player():
     def __init__():
-        inventory = [100,100,100,0,0,0]
-        #           Wood,Stone,Food,Copper,Tin,Iron
-              
-init()
-screen = pygame.display.set_mode((winWidth, winHeight))
-done = False
-while not done:
+        print("")
+        
+
+    def PrintResource():
+        I = inventory
+        WoodText = font.render('Wood : ' + str(int(I[0] )),True,(255,255,255))
+        StoneText = font.render('Stone : ' + str(int(I[1])),True,(255,255,255))
+        FoodText = font.render('Food : ' + str(int(I[2] )),True,(255,255,255))
+        CopperText = font.render('Copper : ' + str(int(I[3] )),True,(255,255,255))
+        TinText = font.render('Tin : ' + str(int(I[4] )),True,(255,255,255))
+        IronText = font.render('Iron : ' + str(int(I[5])),True,(255,255,255))
+        #actually printing...
+        screen.blit(WoodText,(1015,10))
+        screen.blit(StoneText,(1015,30))
+        screen.blit(FoodText,(1015,50))
+        screen.blit(CopperText,(1015,70))
+        screen.blit(TinText,(1015,90))
+        screen.blit(IronText,(1015,110))
+
+def OnScreenRender():
     screen.fill((0,0,0))
     GUI.Render()
+    mouseX, mouseY = pygame.mouse.get_pos()
+    X = int(mouseX/10)
+    Y = int(mouseY/10)
+    XText = font.render('X : ' + str(int(mouseX / 10 )),True,(255,255,255)) # Writes X Position in tile Form
+    YText = font.render('Y : ' + str(int(mouseY / 10 )),True,(255,255,255)) # Writes Y Position in tile Form
+    focusX = int(mouseX / 10)
+    focusY = int(mouseY / 10)
+    print(oreGEN[mouseX + mouseY])
+    screen.blit(XText,(1220,10))
+    screen.blit(YText,(1220,30))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
             pygame.quit()
             exit()
-    mouseX, mouseY = pygame.mouse.get_pos()
-    XText = font.render('X : ' + str(int(mouseX / 20 )),True,(255,255,255)) # Writes X Position in tile Form
-    YText = font.render('Y : ' + str(int(mouseY / 20 )),True,(255,255,255)) # Writes Y Position in tile Form
-
-    focusX = int(mouseX / 20)
-    focusY = int(mouseY / 20)
-    print(oreGEN[mouseX + mouseY])
-    screen.blit(XText,(1050,100))
-    screen.blit(YText,(1125,100))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print (event.button)
+            if event.button == 3:
+                clicked = True
+                GUI.RightClick()
 
     #pygame.draw.rect(screen, (0, 0, 0),  (0, 0, focusX, focusY))
     renderLand()
     clock.tick(60)
+    screen.blit(SelectImage,(X *10,Y *10))
     pygame.display.update()
+    
+init()
+player.__init__()
+screen = pygame.display.set_mode((winWidth, winHeight))
+done = False
+while not done:
+                             
+    OnScreenRender()
+                
+    
 
